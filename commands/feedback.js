992c888ef2d5e5d42d12b5d66f89b53eab204478
Blog <57299745+shinyish@users.noreply.github.com>
2021-12-22 18:47:01 -0800

    Update feedback.js

--- commands/feedback.js
+++ commands/feedback.js
@@ -6,9 +6,21 @@ module.exports = {
     category: "Utility",
     execute(client, message, args) {
         if (args[0]) {
+            const user = message.member
             const feedbackChannel = client.channels.cache.get("923401058577944688")
+            const msg = message.content.split("y!feedback")[1];
 
-            feedbackChannel.send(`**Feedback from <@${message.member.id}>:**\n > ${message.content.split("y!feedback")[1]}`);
+            if (msg.length >= 10) {
+                feedbackChannel.send(`**Feedback from <@${user.id}>:**\n > ${msg}`);
+
+                message.channel.send(`<@${user.id}> Thanks for the feedback! Your feedback was successfully sent to our developers.`)
+                message.delete()
+            } else {
+                message.delete()
+                message.channel.send(`<@${user.id}> Your feedback message must be 10 or more characters.`);
+            }
+        } else {
+            message.channel.send("Invalid arguments ``y!feedback {message}``");
         }
     }
 }
