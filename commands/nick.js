3fc5b01e5b1404963401e76a2ff87b1c9599e34d
Blog <57299745+shinyish@users.noreply.github.com>
2021-12-22 23:49:04 -0800

    Update nick.js

--- commands/nick.js
+++ commands/nick.js
@@ -1,30 +1,5 @@
 const Discord = require("discord.js");
 
-/*
-        if (args[0] && args[1]) {
-            const id = args[0].replace(/[\\<>@#&!]/g, "");
-            const nick = args.slice(1).join(" ");
-
-            if (nick.length <= 30) {
-                if (id == message.member.id) {
-                    if (message.member.permissions.has(Discord.Permissions.FLAGS.CHANGE_NICKNAME) || message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_NICKNAMES)) {
-                        message.member.setNickname(nick)
-                    }
-                } else {
-                    if (message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_NICKNAMES)) {
-                        client.users.fetch(id).then(user => {
-                            user.setNickname(nick);
-                        }).catch(message.channel.send("Error: Couldn't find user."));
-                    }
-                }
-            } else {
-                message.channel.send("Nickname is too long!");
-            }
-        } else {
-            message.channel.send("Invalid arguments ``y!nick {target} {nickname}");
-        }
-*/
-
 module.exports = {
     name: "nick",
     description: "A command that lets you change the nickname of yourself, or others!",
@@ -44,12 +19,16 @@ module.exports = {
                     if (id == message.member.id) {
                         if (message.member.permissions.has(Discord.Permissions.FLAGS.CHANGE_NICKNAME) || message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_NICKNAMES)) {
                             message.member.setNickname(nick)
+                        } else {
+                            message.channel.send("You don't have permission to do that!");
                         }
                     } else {
                         if (message.member.permissions.has(Discord.Permissions.FLAGS.MANAGE_NICKNAMES)) {
                             client.users.fetch(id).then(user => {
                                 user.setNickname(nick);
                             }).catch(message.channel.send("Error: Couldn't find user."));
+                        } else {
+                            message.channel.send("You don't have permission to do that!");
                         }
                     }
                 } else {
