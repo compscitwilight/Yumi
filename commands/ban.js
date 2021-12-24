a6f0829caa4bb9550c85f5a7a7f39d4ddbc61566
Blog <57299745+shinyish@users.noreply.github.com>
2021-12-23 20:07:05 -0800

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -10,10 +10,12 @@ module.exports = {
         if (user.permissions.has(Discord.Permissions.FLAGS.BAN_MEMBERS)) {
             if (args[0] && args[0].includes("<@")) {
                 const target = args[0].replace(/[\\<>@#&!]/g, "")
+                const reason = args.slice(1).join(" ");
 
-                message.guild.members.ban(target)
+                message.guild.members.ban(target, { reason: reason })
+                message.channel.send(`Successfully banned ${target}. Reason: ${reason}`)
             } else {
-                message.channel.send("Invalid arguments ``y!ban {target}``")
+                message.channel.send("Invalid arguments ``y!ban {target} {reason}``")
             }
         } else {
             message.channel.send(`<@${user.user.id}> You are not allowed to run that command! Missing permission(s) BAN_MEMBERS`)
